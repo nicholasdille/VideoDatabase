6d51eab3bfb79d70ec6a9a09cf662da7736359cd
Nicholas Dille <nicholas.dille@mailbox.org>
2016-01-27 20:25:51 +0100

    Added exception to fail consistently

--- maketest.ps1
+++ maketest.ps1
@@ -28,3 +28,31 @@ If ($env:APPVEYOR_JOB_ID) {
 } Else {
     'Skipping Appveyor upload because this job is not running in Appveyor'
 }
+
+$Results = @( Get-ChildItem -Path (Join-Path -Path $here -ChildPath 'tests/*.clixml.result.xml') | Import-Clixml )
+
+$FailedCount = $Results |
+    Select -ExpandProperty FailedCount |
+    Measure-Object -Sum |
+    Select -ExpandProperty Sum
+
+if ($FailedCount -gt 0) {
+    $FailedItems = $Results |
+        Select -ExpandProperty TestResult |
+        Where {$_.Passed -notlike $True}
+
+    "FAILED TESTS SUMMARY:`n"
+    $FailedItems | ForEach-Object {
+        $Test = $_
+        [pscustomobject]@{
+            Describe = $Test.Describe
+            Context = $Test.Context
+            Name = "It $($Test.Name)"
+            Result = $Test.Result
+        }
+    } |
+        Sort Describe, Context, Name, Result |
+        Format-List
+
+    throw "$FailedCount tests failed."
+}
